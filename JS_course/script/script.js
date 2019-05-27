a6eb4541b77f56215de890cdaa8103df88b58590
Dima <riddon04@gmail.com>
2019-05-27 20:33:35 +0300

    This is our first commit

---
+++ JS_course/script/script.js
@@ -0,0 +1,40 @@
+'use srtict';
+
+	var leftBorderWidth = 1;
+	let second = 2;
+	const pi = 3.14;
+
+	console.log(leftBorderWidth);
+
+	let persone = {
+		name: "John",
+		age: "25",
+		isMarried: false
+	};
+
+	console.log(persone.age);
+	console.log(persone['name']);
+
+	let arr = ['plun.png', 'orange.jpg', 'apple.bnp'];
+
+	console.log(arr[2]);
+
+	// alert("hello world");
+
+	// let answer = confirm("are you here?");
+
+	// console.log(answer);
+
+	// let answer = +prompt("Are you here?", "yes");
+
+	// console.log(typeof(answer));
+
+	// console.log("arr" + " - object");
+	// console.log(4 + "arr");
+
+	let incr = 10,
+			decr = 10;
+
+	incr++;
+	decr--;
+
