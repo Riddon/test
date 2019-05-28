a090853346ec2798fefb77279d1434e1ad150868
Dima <riddon04@gmail.com>
2019-05-28 20:29:54 +0300

    This is my mind commit

--- JS_course/script/script.js
+++ JS_course/script/script.js
@@ -1,5 +1,5 @@
 'use srtict';
-
+/* 
 	var leftBorderWidth = 1;
 	let second = 2;
 	const pi = 3.14;
@@ -38,3 +38,38 @@
 	incr++;
 	decr--;
 
+ */
+/* if (2*4 == 7) {
+	console.log("Right!");
+} else {
+	console.log("Wrong!");
+} */
+let num = 50;
+
+if (num < 49) {
+	console.log("Wrong!");
+} else if (num > 100) {
+	console.log("Too mush!");
+} else {
+	console.log("Right!");
+}
+
+(num == 50) ? console.log("Right!") : console.log("Wrong!");
+
+switch (num) {
+	case num < 49:
+		console.log("Wrong!");
+		break;
+	case num > 100:
+		console.log("Too mush!");
+		break;
+	case num > 80:
+		console.log("Too mush again!");
+		break;
+	case 50:
+		console.log("Right!");
+		break;
+	default:
+		console.log("Something going wrong!");
+		break;
+}
