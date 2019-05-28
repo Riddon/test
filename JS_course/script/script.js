14eca9c6371528ca82105da964ca1dbd97451aba
Dima <riddon04@gmail.com>
2019-05-28 23:12:09 +0300

    This is 8 commit

--- JS_course/script/script.js
+++ JS_course/script/script.js
@@ -78,3 +78,23 @@ switch (num) {
 		console.log("Something going wrong!");
 		break;
 } */
+
+let num = 50;
+
+// while (num < 55) {
+// 	console.log(num);
+// 	num++;
+// }
+
+do {
+	console.log(num);
+	num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+	if (i == 6) {
+		continue;
+	}
+	console.log(i);
+}
